679ab7a5327c20c49a3e8f8fe676a5261de2aeac
Afsar Tanvir <afsartanvir805@gmail.com>
2025-11-21 18:43:17 +0600

    Defining A structure for our Movie using Models

---
+++ app/models/movie.ts
@@ -0,0 +1,35 @@
+import MovieService from '#services/movie_service'
+import { toHtml } from '@dimerapp/markdown/utils'
+
+export default class Movie {
+  declare title: string
+
+  declare slug: string
+
+  declare summary: string
+
+  declare abstract?: string
+
+  static async all() {
+    const slugs = await MovieService.getSlugs()
+    const movieList: Movie[] = []
+
+    for (const slug of slugs) {
+      const movie = await this.find(slug)
+      movieList.push(movie)
+    }
+
+    return movieList
+  }
+
+  static async find(slug: string) {
+    const md = await MovieService.read(slug)
+    const movie = new Movie()
+
+    movie.title = md.frontmatter.title
+    movie.summary = md.frontmatter.summary
+    movie.slug = slug
+    movie.abstract = toHtml(md).contents
+    return movie
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 const MoviesController = () => import('#controllers/movies_controller')
 const BooksController = () => import('#controllers/books_controller')
